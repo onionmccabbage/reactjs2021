dd045be29f50bca7fd3df5c1fc7687e130efb46a
onionmccabbage <onionmccabbage@live.com>
2021-07-12 16:09:35 +0100

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Is It Coffee Yet?</h2>
+      <p>This is JSX - JavaScript NOT html</p>
+      <p>lower-case for html tags Initial Cap for React Components</p>
+    </div>
+  );
+}
+
+export default App;
